48f20439fabd6b84c4a16ac8f8b3d3c8c5ec4248
Tiv0w <t.theomeyer@gmail.com>
2020-10-06 10:57:21 +0200

    :wrench: Add eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    'array-bracket-newline': ['warn', 'consistent'],
+    'array-bracket-spacing': ['warn'],
+    'array-element-newline': ['warn', 'consistent'],
+    'block-spacing': ['warn'],
+    'brace-style': ['warn'],
+    'comma-spacing': ['warn'],
+    'comma-style': ['warn'],
+    'eol-last': ['warn'],
+    'func-call-spacing': ['warn'],
+    'key-spacing': ['warn'],
+    'keyword-spacing': ['warn'],
+    'no-console': 'error',
+    'no-lonely-if': ['warn'],
+    'no-multi-spaces': ['warn', { ignoreEOLComments: true }],
+    'no-trailing-spaces': ['warn'],
+    'no-whitespace-before-property': ['warn'],
+    'object-curly-spacing': ['warn', 'always'],
+    'quotes': ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'semi': ['error', 'always'],
+    'space-before-blocks': ['warn'],
+    'space-infix-ops': ['warn'],
+    // ES6
+    'arrow-spacing': ['warn'],
+    'no-var': ['error'],
+    'rest-spread-spacing': ['warn'],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/indent': ['error', 2],
+        '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }]
+      }
+    }
+  ]
+};
