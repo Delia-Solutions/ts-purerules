4492275deacd71ef814bde774ec91ebdcb855ec1
Tiv0w <t.theomeyer@gmail.com>
2020-10-12 00:59:14 +0200

    :art: Improve readability of IPv4 regex

--- src/index.ts
+++ src/index.ts
@@ -48,8 +48,8 @@ function isLeapYear(year: string | number): boolean {
   return (formattedYear % 4 === 0 && formattedYear % 100 !== 0) || formattedYear % 400 === 0;
 }
 
-function stringMustBeValidIpAddress (value: string) {
-  const re = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+function stringMustBeValidIpAddress(value: string) {
+  const re = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
   return re.test(value);
 }
 
